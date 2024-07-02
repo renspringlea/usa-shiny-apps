fad7bc26bfc138dd2ff62fb1d013d8145a320143
renspringlea <ren.springlea@animalask.org>
2024-07-02 16:49:30 +0930

    Update

---
+++ libraries.R
@@ -0,0 +1,12 @@
+
+library(shiny) #For creating the shiny web app
+library(ggplot2) #For graphing
+library(scales) #To help graphing
+library(sf) #For spatial data
+library(viridis) #To help graphing
+library(terra) #for spatial data
+library(plotly) #For interactive graphing
+library(tidyterra) #For graphing spatial data
+
+
+renv::init()
