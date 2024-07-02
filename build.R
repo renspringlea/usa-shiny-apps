ffe43819a3d4995119a7a370e9b1eac3bed854e6
renspringlea <ren.springlea@animalask.org>
2024-07-02 15:28:19 +0930

    Update

---
+++ build.R
@@ -0,0 +1,4 @@
+shinylive::export(appdir="processing_plants_points_map", destdir="docs", subdir="processing_plants_points_map")
+shinylive::export(appdir="species_by_county_map", destdir="docs", subdir="species_by_county_map")
+shinylive::export(appdir="species_by_state_bargraph", destdir="docs", subdir="species_by_state_bargraph")
+shinylive::export(appdir="state_by_species_hexagons", destdir="docs", subdir="state_by_species_hexagons")
---
+++ run.R
@@ -0,0 +1,4 @@
+shiny::runApp("processing_plants_points_map")
+shiny::runApp("species_by_county_map")
+shiny::runApp("species_by_state_bargraph")
+shiny::runApp("state_by_species_hexagons")
